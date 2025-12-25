bcbbc361177547cbff68f628495730c53a908f33
Jan Arick <janarick18@gmail.com>
2025-12-25 22:31:29 +0800

    Connected to live backend

--- flourever-frontend/src/api/axiosConfig.js
+++ flourever-frontend/src/api/axiosConfig.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // Instead of typing "http://localhost:8080" in every file,
 // we can just import this `api` client.
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://flourever-api.onrender.com/api'
 });
 
 // This is an "interceptor" - a bit advanced, but great for your grade.
